67abdc8e04213f178414adfb47eee3738c1787e3
tigeba <tigeba@altereality.com>
2017-12-08 14:30:05 -0600

    delete old unused components.

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Router, Route, browserHistory, withRouter} from "react-router";
-import BotControls from "./BotControls";
 import Main from "./Main";
 import Designer from "./Designer";
 import GamepadSetup from "./GamePadSetup";
@@ -8,7 +7,6 @@ import GamepadSetup from "./GamePadSetup";
 export default  (
     <Router history={browserHistory}>
         <Route path="/" component={withRouter(Main)}>
-            <Route path="/controls" component={BotControls}/>
             <Route path="/designer" component={Designer}/>
             <Route path="/bot/:shortId" component={Main} />
             <Route path="/gamepad" component={GamepadSetup} />
