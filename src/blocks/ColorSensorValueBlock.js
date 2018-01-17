e30ca103daf96bad51bb881075d6adca2a8271be
tigeba <tigeba@altereality.com>
2018-01-16 19:35:22 -0600

    cast result to int.

--- src/blocks/ColorSensorValueBlock.js
+++ src/blocks/ColorSensorValueBlock.js
@@ -1,10 +1,7 @@
 import Blockly from 'node-blockly/browser';
 
-Blockly.JavaScript['color_sensor_value'] = function(block) {
-
+Blockly.JavaScript['color_sensor_value'] = function (block) {
     var dropdown_color = block.getFieldValue('COLOR');
-    // TODO: Assemble JavaScript into code variable.
     var code = dropdown_color;
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.JavaScript.ORDER_NONE];
+    return [parseInt(code), Blockly.JavaScript.ORDER_NONE];
 };
