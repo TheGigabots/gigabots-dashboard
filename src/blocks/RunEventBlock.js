f77e0198bdfdcc69dfac6f0c84ce3d1968d19e5d
tigeba <tigeba@altereality.com>
2018-05-17 16:33:33 -0500

    make the idealized generated js a bit more correct.

--- src/blocks/RunEventBlock.js
+++ src/blocks/RunEventBlock.js
@@ -2,7 +2,7 @@ import Blockly from 'node-blockly/browser';
 
 Blockly.JavaScript['run_event'] = function (block) {
     var func = [];
-    func.push('onRun = async () => {');
+    func.push('let onRun = async () => {');
     func.push(Blockly.JavaScript.statementToCode(block, 'BODY'));
     func.push('}');
     return func.join('\n');
--- src/blocks/StartEventBlock.js
+++ src/blocks/StartEventBlock.js
@@ -3,7 +3,7 @@ import Blockly from 'node-blockly/browser';
 
 Blockly.JavaScript['start_event'] = function (block) {
     var func = [];
-    func.push('onStart = (done) => {');
+    func.push('let onStart = (done) => {');
     func.push(Blockly.JavaScript.statementToCode(block, 'BODY'));
     func.push('  done(); \n');
     func.push('}');
